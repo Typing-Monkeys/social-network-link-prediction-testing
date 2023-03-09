bda88339984e586dd16ae3a888f27ae87603b580
CristianCosci <cristiancosci@gmail.com>
2023-03-09 18:02:25 +0100

    Primo Commit
    Inizializzazione per testpyPI

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='social_network_link_prediction',
+    version='0.1',
+    license='MIT',
+    author="Cristian Cosci, Fabrizio Fagiolo, Nicolò Vescera, Nicolò Posta, Tommaso Romani",
+    author_email='email@example.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Typing-Monkeys/social-network-link-prediction',
+    keywords='example project',
+    install_requires=[
+          'numpy',
+      ],
+
+)
